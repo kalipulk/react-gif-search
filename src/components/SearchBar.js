fd906b6219e8b35a2965003c18cd3ef3f424a145
Kali Pulkkinen <kapulkkinen@gmail.com>
2020-10-09 13:51:53 -0400

    inital file structure with some components in progress

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const SearchBar = ( {searchTerm, changeSearchTerm, getGifs } ) => {
+//     return (
+//         <div>
+//             <input ClassName="userSearch" type="text" placeholder="search for gifs..." value={searchTerm} onChange={changeSearchTerm}
+//             />
+//         </div>
+//     )
+// }
+// export default SearchBar
